6f46e9212c1d3430b48d13da50af4f6da9cec97e
Mattijs Ugen <akaidiot@users.noreply.github.com>
2017-08-30 17:27:39 +0200

    Use new client API, send list of dicts

--- ledder.py
+++ ledder.py
@@ -8,8 +8,7 @@ from requests import Session
 
 
 def _glue_url(*parts):
-    # append trailing slash by ending with an empty string (for peter's sake :))
-    return path.join(*(str(part) for part in parts), '')
+    return path.join(*(str(part) for part in parts))
 
 
 class LedderClient:
@@ -17,38 +16,59 @@ class LedderClient:
         self.endpoint = endpoint
         self.session = Session()
 
-    def put_led(self, loc, color):
-        x, y = loc
-        self.session.put(_glue_url(self.endpoint, 'leds', x, y), json=list(color))
+    def put_all(self, leds):
+        response = self.session.put(_glue_url(self.endpoint, 'ledstrip'), json=leds)
+        assert response.status_code == 200, 'unexpected response code: {}: {}'.format(
+            response.status_code,
+            response.reason
+        )
+
+
+def rows(pixels, width):
+    for start in range(0, len(pixels), width):
+        yield pixels[start:start + width]
 
 
 def to_pixels(buffer, target_size):
+    # load image, create a thumbnail from it
     image = Image.open(buffer)
-    # TODO: preserve aspect ratio, insert empty lines as to center
-    image = image.resize(target_size)
-    image.show()
+    image.thumbnail(target_size)
 
-    data = image.getdata()
+    target_width, target_height = target_size
+    # image is to be centered, calculate x and y offsets
+    target_x = (target_width - image.width) // 2
+    target_y = (target_height - image.height) // 2
+
+    # create new image of target size
+    result = Image.new('RGB', target_size)
+    # paste the thumbnail into it, at an offset
+    result.paste(image, (target_x, target_y))
+
     # strip anything outside rgb
-    return tuple((r, g, b) for r, g, b, *_ in data)
+    return tuple((r, g, b) for r, g, b, *_ in result.getdata())
 
 
 def main(args):
     with open(args.image, 'rb') as image:
-        pixels = to_pixels(image, (12, 10))
+        pixels = to_pixels(image, (args.width, args.height))
+        assert len(pixels) == args.width * args.height, 'refusing to upscale images'
 
     client = LedderClient(args.endpoint)
-    for idx, pixel in enumerate(pixels):
-        x = idx % 12
-        y = idx // 10
-        print('setting pixel ({}, {}) to {}'.format(x, y, pixel))
-        client.put_led((x, y), pixel)
+    # construct data, list of dicts with an rgb value for an x/y coord
+    leds = [{'x': idx % args.width,
+             'y': idx // args.width,
+             'rgb': rgb}
+            for idx, rgb in enumerate(pixels)]
+    # push all led data to remote
+    client.put_all(leds)
 
 
 if __name__ == '__main__':
     parser = ArgumentParser(description='render picture file to tuintrip led matrix')
     parser.add_argument('image')
-    parser.add_argument('-e', '--endpoint', default='http://192.168.1.25:5000')
+    parser.add_argument('--endpoint', default='http://192.168.1.25:5000')
+    parser.add_argument('--width', type=int, default=12)
+    parser.add_argument('--height', type=int, default=10)
 
     args = parser.parse_args()
     main(args)
